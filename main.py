16d51808d8d194018c48bf679e3256937fc2da6c
Colin Parrott <colinparrott2@gmail.com>
2022-01-30 03:53:05 +0000

    Improve config reading error handling

--- main.py
+++ main.py
@@ -11,11 +11,22 @@ CONFIG_PATH = sys.path[0] + os.sep + "flasher_config.json"
 
 
 def read_config() -> Dict:
+    if not Path(CONFIG_PATH).is_file():
+        exit_program("Config file does not exist at %s" % CONFIG_PATH)
+
     with open(CONFIG_PATH, 'r') as f:
         return json.load(fp=f)
 
 
 def validate_config(cfg: dict) -> bool:
+
+    if "compiled_firmware_path" not in cfg:
+        exit_program("'compiled_firmware_path' is missing in  the config file (%s)" % CONFIG_PATH)
+    elif "target_firmware_name" not in cfg:
+        exit_program("'target_firmware_name' is missing in  the config file (%s)" % CONFIG_PATH)
+    elif "target_drive" not in cfg:
+        exit_program("'target_drive' is missing in  the config file (%s)" % CONFIG_PATH)
+
     if not Path(cfg["compiled_firmware_path"]).is_file():
         exit_program("compiled_firmware_path does not exist")
         return False
